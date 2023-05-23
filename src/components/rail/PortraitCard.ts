1d9806780e36d5a490bcbec2ef53cffa6982ab20
Mohammad Shariq <shariqsway@gmail.com>
2023-05-23 21:31:44 +0530

    Display fallback placeholder for UI consistency

--- src/components/rail/PortraitCard.ts
+++ src/components/rail/PortraitCard.ts
@@ -10,6 +10,10 @@ export class PortraitCard extends lng.Component {
       Image: {
         w: 158,
         h: 237,
+        texture: {
+          type: lng.textures.ImageTexture,
+          src: "",
+        },
         shader: {
           type: lng.shaders.RoundedRectangle,
           radius: 10,
@@ -17,13 +21,18 @@ export class PortraitCard extends lng.Component {
       },
       Label: {
         x: 0,
-        y: 255,
+        y: 248,
+        w: 158,
         color: Color.text,
         text: {
           text: "",
           fontSize: 18,
           fontStyle: FontWeight.Regular,
           fontFace: "TitilliumWeb",
+          lineHeight: 22,
+          wordWrap: true,
+          maxLines: 2,
+          maxLinesSuffix: "...",
         },
       },
       Description: {
@@ -38,7 +47,11 @@ export class PortraitCard extends lng.Component {
   set item(movie: { src: string; label: string; description: string }) {
     const { src, label, description } = movie;
     this.patch({
-      Image: { src },
+      Image: {
+        texture: {
+          src,
+        },
+      },
     });
     this.patch({
       Label: {
@@ -48,6 +61,23 @@ export class PortraitCard extends lng.Component {
     this.patch({
       Description: {
         text: description,
+      },
+    });
+  }
+
+  _init() {
+    /* Display fallback placeholder */
+    this.tag("Image").on("txError", () => {
+      this.displayPlaceholder();
+    });
+  }
+
+  displayPlaceholder() {
+    this.patch({
+      Image: {
+        texture: {
+          src: "./static/images/placeholder.png",
+        },
       },
     });
   }
